e863ecfe160bca60b57afee19a44c1571b62997c
Chris McKnight <cmckni3@gmail.com>
2018-02-14 00:10:32 -0600

    chore(create-project): Create symfony 4 project

---
+++ tests/SiteTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SiteTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
